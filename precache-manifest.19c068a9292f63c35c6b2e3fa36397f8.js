d67a1453bc81549a8b93bf349d1375583397c950
Amish Rasheed <amishrasheed22@gmail.com>
2021-06-19 20:56:22 -0400

    Updates

---
+++ precache-manifest.19c068a9292f63c35c6b2e3fa36397f8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c549b62dbab1a082fb1914c15354eff",
+    "url": "/searchflix/index.html"
+  },
+  {
+    "revision": "8aa3f620526ea3f0d088",
+    "url": "/searchflix/static/css/2.65e8e33d.chunk.css"
+  },
+  {
+    "revision": "9957460a30d26884d878",
+    "url": "/searchflix/static/css/main.b500a862.chunk.css"
+  },
+  {
+    "revision": "8aa3f620526ea3f0d088",
+    "url": "/searchflix/static/js/2.14383a28.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/searchflix/static/js/2.14383a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9957460a30d26884d878",
+    "url": "/searchflix/static/js/main.4c6ddea2.chunk.js"
+  },
+  {
+    "revision": "fdc3b743082cd19a506c",
+    "url": "/searchflix/static/js/runtime-main.9a9507bd.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/searchflix/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "44292c8e4fe589eeb56f550aef4d1fc3",
+    "url": "/searchflix/static/media/none.44292c8e.jpg"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/searchflix/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/searchflix/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/searchflix/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/searchflix/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
